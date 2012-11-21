0cb6aff5c4ffe2ce7c622e852589181742135865
Jamie Good <jgood@paddpower.com>
2012-11-21 17:34:06 +0000

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  
+  
+  def index
+    
+    @tasks = Task.all
+    
+    puts @tasks
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @tasks }
+    end    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '76291293c257d0f4553f5a83c478f4505ae9e393d4a908cbd807c94c30bcc77007620fcf457fc4bb4dc41651ffe8928ec2c8894b1a4d82db29c088d106054f84'
